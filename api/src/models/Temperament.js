215a0adc7b57bb3fde66165b2d78b478eebd6107
[Saschi] <saschenkagonz@outlook.com>
2022-07-12 15:45:36 -0400

    id datatype changed

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -5,8 +5,9 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("temperament", {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      // allowNull: false,
       primaryKey: true,
     },
     name: {
